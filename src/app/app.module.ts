d902b1a358d44f2c5b13beb0c5fadb10214e74cf
UPPERLINK <you@example.com>
2019-03-27 11:44:14 +0100

    My first angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
